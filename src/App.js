6d8a62d89635f7972855a6627a90f8edfa82e407
Burak Kocak <burakkocak884@ghotmail.com>
2019-10-01 17:46:06 -0400

    navbar halfway done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GadgetListContainer from './containers/GadgetListContainer'
-
+import {BrowserRouter as Router,Route, Switch, Link} from 'react-router-dom';
+import Calculator from './components/Calculator'
+import Welcome from './components/Welcome'
 function App() {
   return (
-    <div >
-      <p>Welcome to Mr Gadget</p>
-      <GadgetListContainer />
+   
+     
+        <div className='navbar'>Home
+          <ul className='menu'>
+           
+          <li><a href='' className='fa fa-calculator'></a></li>
+          <li><a href='' className='fa fa-facebook'></a></li>
+          <li><a href='' className='fa fa-twitter'></a></li>
+          <li><a href='' className='fa fa-linkedin'></a></li>
+          <li><a href='' className='fa fa-github'></a></li>
+          <li><a href='' className='fa fa-calculator'></a></li>
+          <li><a href='' className='fa fa-facebook'></a></li>
+          <li><a href='' className='fa fa-twitter'></a></li>
+          <li><a href='' className='fa fa-linkedin'></a></li>
+          <li><a href='' className='fa fa-github'></a></li>
+
+          </ul>
+       
+     
+      
+    
       
       </div>
+     
   );
 }
 
---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Welcome =() => {
+    return (
+        <div><p>Welcome Home</p></div>
+    )
+}
+export default Welcome
