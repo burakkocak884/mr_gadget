7b470e9438ce9c2f69c7055954dd470ec2365afc
Burak Kocak <burakkocak884@ghotmail.com>
2019-10-02 11:06:33 -0400

    navbar alterations

--- src/App.js
+++ src/App.js
@@ -6,30 +6,31 @@ import Calculator from './components/Calculator'
 import Welcome from './components/Welcome'
 function App() {
   return (
-    <div>
+    <Router>
+      
+    
    
      
-        <div className='navbar'>Home
+        <div className='navbar'>
+          <div className='selected-tool'></div>
           <ul className='menu'>
            
-          <li><a href='' className='fa fa-calculator'></a></li>
-          <li><a href='' className='fa fa-facebook'></a></li>
-          <li><a href='' className='fa fa-twitter'></a></li>
-          <li><a href='' className='fa fa-linkedin'></a></li>
-          <li><a href='' className='fa fa-github'></a></li>
+          <li><Link to='/calculator' className='fa fa-calculator'></Link></li>
+          <li><a href='https://www.facebook.com/burak.kocak.12935' className='fa fa-facebook' target="_blank"></a></li>
+          <li><a href='https://twitter.com/BurakKo26718607' className='fa fa-twitter' target="_blank"></a></li>
+          <li><a href='https://www.linkedin.com/in/burak-kocak-3bb572141' className='fa fa-linkedin' target="_blank"></a></li>
+          <li><a href='https://github.com/burakkocak884' className='fa fa-github' target="_blank"></a></li>
           <li><a href='' className='fa fa-calendar'></a></li>
           <li><a href='' className='fa fa-email'></a></li>
           <li><a href='' className='fa fa-flag'></a></li>
           
 
           </ul>
-       
-     
-      
-    
-      
-      </div>
-      </div>
+       </div>
+       <Switch>
+       <Route path='/calculator' render={()=>(<Calculator />)}></Route>
+      </Switch>
+      </Router>
      
   );
 }
