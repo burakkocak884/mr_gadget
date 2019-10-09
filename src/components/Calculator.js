e22a2d47fa285967bfa025285c2afd1346f17209
Burak Kocak <burakkocak884@ghotmail.com>
2019-10-09 11:59:18 -0400

    calculator more repair

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,115 +6,110 @@ import {connect} from 'react-redux';
 import {calcInput} from '../action/Tool'
   class Calculator extends Component{
     constructor(props){
-        super(props)
-        
-        this.state ={
-            placeHolder: '',
-            screen: ''
+                super(props)
+                
+                this.state ={
+                    placeHolder: '',
+                    screen: ''
+                 }
+                 this.handleDelete = this.handleDelete.bind(this);
+             }
 
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange = event =>{
-        console.log('valid value =',event.target.value)
-        //debugger
-         this.setState({ placeHolder: this.state.placeHolder + event.target.value
-        })
-     }
-     handleClear = event =>{
-         this.setState({
-             placeHolder: ''
-         })
-     }
 
-    handleSubmit = event =>{
-        event.preventDefault()
-       this.props.calcInput(this.state.placeHolder)
-        
-    }
+                handleChange = event =>{
+                    //console.log('valid value =',event.target.value)
+                    //debugger
+                    this.setState({ placeHolder: this.state.placeHolder + event.target.value})
+                }
+
+
+                handleClear = event =>{
+                    this.setState({placeHolder: ''})
+                }
+
+
+                handleDelete = event =>{
+                    let theString = this.state.placeHolder
+                    this.setState({placeHolder: theString.slice(0,theString.length-1) })
+                // console.log('the state=',this.state)
+                }
+
+
+                handleSubmit = event =>{
+                    event.preventDefault()
+                this.props.calcInput(this.state.placeHolder)
+                }
     
 
 
-    render(){
-        console.log('props=',this.props.message)
-        let display; 
-        if(this.props.message){
-                display = this.props.message.toString()
-        }else if(this.props.result){
-            display = this.props
-        }
-        return(
-           
-            
+                    render(){
+                        // console.log('props=',this.props.message)
+                        let display; 
 
-                <div className='calculator'>
-                     
-                     <Form >
-                        <Form.Field>
-                            <label>Input</label>
-                            <input  type='textarea' onChange={this.handleChange} name ='city' placeholder='Type or Click' value={this.state.placeHolder}></input>
-                           <label>Output</label>
-                            <input  type='textarea' onChange={this.handleChange} name ='city' placeholder='Result' value={display}></input>
-                        </Form.Field>
-                        </Form>
-                    <div class="ui inverted segment"> 
-                      
-                  
-                    <table >
-                  
-                    <tr>
-                    <td><button  className='ui inverted button' onClick={this.handleClear} value={'clear'}>C</button></td>
-                    <td><button  className='ui inverted button' onClick={this.handleChange} value={'%'}>%</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={'%'}>Mod</button></td>
-                    <td><button  className='ui inverted button' onClick={this.handleChange} value={'+'}>+</button></td>
-                    </tr>
-                    <tr>
-                    <td><button  className='ui inverted button' onClick={this.handleChange} value={1}>1</button></td>
-                    <td><button  className='ui inverted button' onClick={this.handleChange} value={2}>2</button></td>
-                    <td><button  className='ui inverted button' onClick={this.handleChange} value={3}>3</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={'-'}>-</button></td>
-                    </tr>
+                        if(this.props.message){
+                                display = this.props.message.toString()
+                        }else if(this.props.result){
+                            display = this.props.result
+                        }
+                        return(
+                        
+                            
 
-                    <tr>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={4}>4</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={5}>5</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={6} >6</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={'*'} >*</button></td>
-                    </tr>
+                                <div className='calculator'>
+                                    
+                                    <Form >
+                                            <Form.Field>
+                                            
+                                            <label>Input</label>
+                                                <input  type='textarea' onChange={this.handleChange} name ='city' placeholder='Type or Click' value={this.state.placeHolder}></input>
+                                                
+                                                <label>Output</label>
+                                            
+                                                <input  type='textarea' onChange={this.handleChange} name ='city' placeholder='Result' value={display}></input>
+                                            </Form.Field>
+                                     </Form>
+                                             <div class="ui inverted segment"> 
+                                    
+                                
+                                                <table >
+                                
+                                                    <tr>
+                                                        <td><button  className='ui inverted button' onClick={this.handleClear} value={'clear'}> C </button></td>
+                                                        <td><button  className='ui inverted button' onClick={this.handleDelete} value={'%'}>Del</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={'%'}>Mod</button></td>
+                                                        <td><button  className='ui inverted button' onClick={this.handleChange} value={'+'}>+</button></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><button  className='ui inverted button' onClick={this.handleChange} value={1}>1</button></td>
+                                                        <td><button  className='ui inverted button' onClick={this.handleChange} value={2}>2</button></td>
+                                                        <td><button  className='ui inverted button' onClick={this.handleChange} value={3}>3</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={'-'}>-</button></td>
+                                                    </tr>
 
-                    <tr>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={7} >7</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={8} >8</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={9} >9</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={"/"} >\</button> </td>
-                    </tr>
-                    <tr>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={'('} >(</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={0} >0</button></td>
-                    <td><button className='ui inverted button' onClick={this.handleChange} value={')'} >)</button></td>
-                    <td><button type='submit' className='ui inverted button' onClick={this.handleSubmit} value={'equal'} >=</button></td>
-                    </tr>
+                                                    <tr>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={4}>4</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={5}>5</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={6} >6</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={'*'} >*</button></td>
+                                                    </tr>
 
-
-
-
-
-
-                </table>
-              
-</div>
-
-
-               
-
-               
-                
-               
-                
-                
-                </div>
-        )
-    }
+                                                    <tr>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={7} >7</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={8} >8</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={9} >9</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={"/"} >\</button> </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={'('} >(</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={0} >0</button></td>
+                                                        <td><button className='ui inverted button' onClick={this.handleChange} value={')'} >)</button></td>
+                                                        <td><button type='submit' className='ui inverted button' onClick={this.handleSubmit} value={'equal'} >=</button></td>
+                                                    </tr>
+                                                 </table>
+                            
+                                               </div>
+                                            </div>
+        )}
 
 }
 
--- src/reducers/manageTool.js
+++ src/reducers/manageTool.js
@@ -10,21 +10,20 @@ export default function manageTool(state = {myweather: '', calcInput: ''
        return {myweather: action.foundInfo};
 
      case 'CALC_INPUT':
-        let char = action.input
-        let regex = /^\s*([-+]?)(\d+)(?:\s*([-+*\/])\s*((?:\s[-+])?\d+)\s*)+$/
-        let cond =regex.test(char)
-        let chAry = action.input.split('')
-        let errorMessage ;
-        let result = 0;
-      
-        if(cond === true){
-              result = chAry.join('')
-              result = eval(result)
-             return result;
-        }else{
-               errorMessage = "Wrong Input, try again";
-            return errorMessage;
-        }
+        let theString = action.input
+       
+              try {
+               debugger
+               return {result: eval(theString)}; 
+            } catch (e) {
+                if (e instanceof SyntaxError) {
+                    alert('Wrong input');
+                } else {
+                    throw( e );
+                }
+            }
+   
+        
         
       default:
          return state;
