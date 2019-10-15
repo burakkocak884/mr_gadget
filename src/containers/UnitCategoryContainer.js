e4cbb7016fe2afe6a4c7141adf7ae3d41a524756
Burak Kocak <burakkocak884@ghotmail.com>
2019-10-14 22:51:30 -0400

    unit convertor at work

--- src/containers/UnitCategoryContainer.js
+++ src/containers/UnitCategoryContainer.js
@@ -25,44 +25,60 @@ class UnitCategoryContainer extends Component {
 
     
     //const units = ['Area','Fuel Economy','Length','Mass','Speed','Temperature','Time','Volume']
-    handleChange = (event) =>{
-       
-        
-        var cat = this.state.units.category
-        var myUnitTypes = cat[event.target.value]
-        this.setState({
-            unitTypes: myUnitTypes
-        })
-        //console.log('event',event.target.value)
-        //debugger
-    }
-    handleConversion1 = (event) =>{
-        
-        this.setState({
-            firstSubUnit: event.target.value
-        })
-        debugger
+            handleChange = (event) =>{
+            var cat = this.state.units.category
+                var myUnitTypes = cat[event.target.value]
+                this.setState({
+                    unitTypes: myUnitTypes, 
+                    firstSubUnit: '',
+                    secondSubUnit: ''
+                })
+            }
 
-    }
-    handleConversion2 = (event) =>{
-        this.setState({
-            secondSubUnit: event.target.value
-        })
-       
+            handleSelect1 = (event) =>{
+                this.setState({ firstSubUnit: event.target.value })
+               
+            }
+
+            handleSelect2 = (event) =>{
+                this.setState({ secondSubUnit: event.target.value })
+            }
+
+            handleInput1 = event =>{
+                console.log('state in handleinput1=',this.state)
+            
+                 let value = event.target.value
+                 let firstUnit = this.state.firstSubUnit
+                 let secondUnit = this.state.secondSubUnit
+                 let input1 = this.state.input1
+                 let input2 = this.state.input2
+                //  if(firstUnit === secondUnit){
+                //     this.setState({input1: value, input2: value})
+                //  }else if (firstUnit === "Kilometer" && secondUnit === "Meter"){
+                //     this.setState({ input2: value*1000})
+                //  }
+                // while( value){
+                //     debugger
+                //     switch(input1 && input2){
+                //         case firstUnit === secondUnit:
+                //         this.setState({input1: value, input2: value})
+                //         debugger
+                //         break;
+                //         case firstUnit === "Kilometer" && secondUnit === "Meter":
+                            
+                //         this.setState({ input2: value*1000})
+                //         debugger
+                //         break;
+                //     }}
+                debugger
+                    
+                }
+
+            handleInput2 = event =>{
+                this.setState({ input1: event.target.value })
+            }
+
         
-
-    }
-    handleInput1 = event =>{
-        this.setState({
-            input2: event.target.value
-        })
-
-    }
-    handleInput2 = event =>{
-        this.setState({
-            input1: event.target.value
-        })
-    }
 
 
 
@@ -75,31 +91,25 @@ class UnitCategoryContainer extends Component {
                 
                 
                 <select name="category" multiple="" class="ui fluid dropdown" onChange={this.handleChange} >
-                <option value="">Select A Unit Category</option>
-
-                      {categories.map(k=>(
-                         <option value={`${k}`}>{k}</option>
-                      ))}
+                                <option value="">Select A Unit Category</option>
+                                {categories.map(k=>( <option value={`${k}`}>{k}</option> ))}
                    </select>
                    <Form>
                           <Form.Field>
                               <input placeholder='first box'  value={this.state.input1} onChange={this.handleInput1}></input>
-                                    <select name="first" multiple="" class="ui fluid dropdown" onChange={this.handleConversion1}>
-                                    <option value="">Select A Unit</option>
-                                    {this.state.unitTypes.map(c=>(
-                         <option valueFirst={`${c}`}>{c}</option>
-                      ))}
+                                    <select name="first" multiple="" class="ui fluid dropdown" onChange={this.handleSelect1}>
+                                        <option value="">Select A Unit</option>
+                                        {this.state.unitTypes.map(c=>(
+                                        <option valueFirst={`${c}`}>{c}</option>
+                                       ))}
                                     </select>
                                <input placeholder='second box'  value={this.state.input2} onChange={this.handleInput2}></input>
-                                    <select name="second" multiple="" class="ui fluid dropdown" onChange={this.handleConversion2}>
-                                    <option value="">Select A Unit</option>
-                                    {this.state.unitTypes.map(c=>(
-                         <option valueSecond={`${c}`}>{c}</option>
-                      ))}
-                                  
+                                    <select name="second" multiple="" class="ui fluid dropdown" onChange={this.handleSelect2}>
+                                        <option value="">Select A Unit</option>
+                                        {this.state.unitTypes.map(c=>( <option valueSecond={`${c}`}>{c}</option> ))}
                                     </select>
                           </Form.Field>
-                      </Form>
+                    </Form>
 
             </div>
 
