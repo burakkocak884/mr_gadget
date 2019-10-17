5b02301b7226233e0467dcaa331c92c5509c7e9f
Burak Kocak <burakkocak884@ghotmail.com>
2019-10-17 13:35:40 -0400

    api key location

--- src/action/Tool.js
+++ src/action/Tool.js
@@ -4,11 +4,13 @@ export const findWeather = zipCode => {
       return (dispatch) => {
         //dispatch({ type: 'START_GETTING_WEATHER_INFO' });
         let API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-     
-        return fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`)
+      const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}`
+
+      //api.openweathermap.org/data/2.5/weather?zip=30022&appid=4a580b938e7bce4964ca8d24e3503b3c
+        return fetch(url)
           .then(response => response.json())
           .then(foundInfo =>  {
-            //console.log('returned info= ',foundInfo)
+            console.log('returned info= ',foundInfo)
             
             dispatch({ type: 'FOUND_INFO', foundInfo})
            
