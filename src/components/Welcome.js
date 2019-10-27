f34907b1d89c21f575c94eeedbe34da0bb4ee549
Burak Kocak <burakkocak884@hotmail.com>
2019-10-26 23:52:50 -0400

    email sender added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Welcome =() => {
     return (
         <div className='welcome'>
-            <p>Thank for checking out</p>
+            <p>Thank you for checking out</p>
             <span className='welcome-a'><p> Mr Gadget</p></span>
             <p> created by <span className='welcome-b'>Burak Kocak</span></p>
+            <p><a href="mailto:burakkocak884@gmail.com" target="_blank" rel="noopener noreferrer"><i class="envelope icon"></i></a></p>
 
         </div>
     )
