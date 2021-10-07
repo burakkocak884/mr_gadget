62d0afddaf501436d933e5490e83b4790443ef08
Burak Kocak <burakkocak884@gmail.com>
2021-10-07 13:26:26 -0400

    api error handled

--- src/action/Tool.js
+++ src/action/Tool.js
@@ -1,10 +1,9 @@
 
 // findWeather function will fecth the data from external API with given zipCode
     export const findWeather = zipCode => {
-        return (dispatch) => {
-        
-            let API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+          const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
           const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}`
+        return (dispatch) => {
             return fetch(url)
               .then(response => response.json())
               .then(foundInfo =>  {
